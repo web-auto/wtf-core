7624fd0f2cf23f5fde56c6eba38e4b6047ddf850
Venkat Sundramurthy <venkatesan.sundramurthy@gmail.com>
2016-02-10 09:13:15 -0800

    Update BaseAPIClient.java

--- src/main/java/org/wtf/core/feature/api/BaseAPIClient.java
+++ src/main/java/org/wtf/core/feature/api/BaseAPIClient.java
@@ -31,16 +31,6 @@ public class BaseAPIClient extends BaseHttpClient {
   public String request(String requestXML) throws IOException {
     Map<String,String> propertiesMap = new HashMap<String, String>();
     propertiesMap.put("Content-Type", "application/xml ");
-    propertiesMap.put("X-EBAY-API-CERT-NAME", "AdminCertificate");
-    propertiesMap.put("X-EBAY-API-CALL-NAME", "AddItem");
-    propertiesMap.put("X-EBAY-API-SESSION-CERTIFICATE", "Admintest1;AdminApp;AdminCertificate");
-    propertiesMap.put("X-EBAY-API-SITEID", "100");
-    propertiesMap.put("X-EBAY-API-COMPATIBILITY-LEVEL", "739");
-    propertiesMap.put("X-EBAY-API-DETAIL-LEVEL", "0");
-    propertiesMap.put("X-EBAY-API-DEV-NAME", "Admintest1");
-    propertiesMap.put("X-EBAY-API-FLAGS", "SDK1.60.2358.27060");
-    propertiesMap.put("X-EBAY-API-APP-NAME", "AdminApp");
-    propertiesMap.put("X-EBAY-API-RESPONSE-ENCODING", "XML");
     String responseXML = super.request(propertiesMap, requestXML);
     LOG(Level.INFO, String.format("API Response: %s", responseXML));
     return responseXML;
